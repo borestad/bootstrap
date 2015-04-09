068d9eadb25def9e391806385d793e52a225b2a9
Johan Borestad <johan@borestad.com>
2015-04-09 18:26:32 +0200

    / ‘os/centos/centos7/bootstrap.sh’

--- os/centos/centos7/bootstrap.sh
+++ os/centos/centos7/bootstrap.sh
@@ -6,3 +6,4 @@ sudo yum install wget
 echo "Install linuxbrew"
 sudo yum groupinstall 'Development Tools' && sudo yum install -y curl git irb m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+brew doctor
