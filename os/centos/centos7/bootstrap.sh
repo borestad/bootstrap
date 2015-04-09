26571eb9b3df0798e4ffcfdeb7b84ff39c1fe97e
Johan Borestad <johan@borestad.com>
2015-04-09 18:45:53 +0200

    / ‘os/centos/centos7/bootstrap.sh’

--- os/centos/centos7/bootstrap.sh
+++ os/centos/centos7/bootstrap.sh
@@ -3,22 +3,22 @@
 echo "Update system? (Press any key to continue)"
 sudo yum update
 
-# echo "Install wget? (Press any key to continue)"
-# sudo yum install wget
+echo "Install wget? (Press any key to continue)"
+sudo yum install wget
 
 echo "Install git? (Press any key to continue)"
 sudo yum install git
 git clone git@github.com:borestad/bootstrap.git ~/.bootstrap
 
-echo "Install linuxbrew? (Press any key to continue)"
-read -r
-sudo yum groupinstall 'Development Tools' && sudo yum install -y curl git irb m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+# echo "Install linuxbrew? (Press any key to continue)"
+# read -r
+# sudo yum groupinstall 'Development Tools' && sudo yum install -y curl git irb m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
 
-echo "Installing PATH to .bash_profile? (Press any key to continue)"
-read -r
-echo export PATH='~/.linuxbrew/bin:$PATH' >> ~/.bash_profile
-source ~/.bash_profile
+# echo "Installing PATH to .bash_profile? (Press any key to continue)"
+# read -r
+# echo export PATH='~/.linuxbrew/bin:$PATH' >> ~/.bash_profile
+# source ~/.bash_profile
 
-echo "Running brew doctor (Press any key to continue)"
-brew doctor
+# echo "Running brew doctor (Press any key to continue)"
+# brew doctor
