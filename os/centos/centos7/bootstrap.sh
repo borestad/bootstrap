1c49267cbe31fdfcc6b40c2d0d648c65d5a6acc0
Johan Borestad <johan@borestad.com>
2015-04-09 18:02:33 +0200

    / ‘os/centos/centos7/bootstrap.sh’

--- os/centos/centos7/bootstrap.sh
+++ os/centos/centos7/bootstrap.sh
@@ -2,4 +2,6 @@
 
 sudo yum update
 sudo yum install wget
+
+echo "Install linuxbrew"
 sudo yum groupinstall 'Development Tools' && sudo yum install curl git irb m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel
